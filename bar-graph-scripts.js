56dbb3b6ab13a3c2aaba81238cb8eda3b375a62e
Diego Castro <diegojcr4@gmail.com>
2021-07-27 15:07:38 -0400

    added dynamic sizing of width, columns stacked vertically. TODO: figure out how to make them add horizontally instead

--- bar-graph-scripts.js
+++ bar-graph-scripts.js
@@ -2,7 +2,37 @@ $(document).ready(function(){
   $("#paragraphToggle").click(function(){
     $("p").toggle(200);
   });
+
   $("#updateTitle").click(function() {
     $("#graphTitle").text( $("#titleForm").val() );
   });
+
+  // Why doesn't it let me use 'let' instead of 'var'?
+  var myData = [1, 2, 3, 4, 3];
+  myData.push(5);
+
+  $("#getData").click(function() {
+    var convertedData = "";
+    for (var dataEntry of myData) {
+      convertedData += dataEntry;
+    }
+    $("#placeholderText").text(convertedData);
+
+    $("#firstBar").width(25);
+
+    var numBars = myData.length;
+    for(let barCount = 0; barCount < numBars; barCount++) {
+      let newBar = document.createElement("div");
+      $(newBar).addClass("bar");
+      let titleNum = myData[barCount];
+      $(newBar).attr({
+        "title": titleNum
+      });
+      $(newBar).width(myData[barCount]*10);
+      $("#firstBar").after(newBar);
+
+    }
+
+  });
+
 });
