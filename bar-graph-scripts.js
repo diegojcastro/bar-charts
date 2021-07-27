dc5bc55ccfc0b5d2e7c97f9040b22b4ef853c019
Diego Castro <diegojcr4@gmail.com>
2021-07-27 13:34:26 -0400

    added function to change graph title

--- bar-graph-scripts.js
+++ bar-graph-scripts.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $("#paragraphToggle").click(function(){
     $("p").toggle(200);
   });
+  $("#updateTitle").click(function() {
+    $("#graphTitle").text( $("#titleForm").val() );
+  });
 });
