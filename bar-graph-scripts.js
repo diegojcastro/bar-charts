ff1d9e2d42ef142c8097d4a9b50051da305b5af6
Diego Castro <diegojcr4@gmail.com>
2021-08-02 09:29:21 -0400

    added display:flex and justify:center to HTML of chart area. Now bars stack horizontally.

--- bar-graph-scripts.js
+++ bar-graph-scripts.js
@@ -21,9 +21,19 @@ $(document).ready(function(){
     $("#firstBar").width(25);
 
     var numBars = myData.length;
+    // Quick pass to get the highest value for %size calculations
+    let maxValue = 0;
+    for (let i = 0; i < numBars; i++ ) {
+      if (myData[i] > maxValue) {
+        maxValue = myData[i];
+      }
+    }
+
+
     for(let barCount = 0; barCount < numBars; barCount++) {
       let newBar = document.createElement("div");
       $(newBar).addClass("bar");
+      // titleNum will be assigned to "title" attribute on mouseover
       let titleNum = myData[barCount];
       $(newBar).attr({
         "title": titleNum
@@ -32,6 +42,13 @@ $(document).ready(function(){
       $("#firstBar").after(newBar);
 
     }
+
+    //Manual test of a last bar:
+    let lastBar = document.createElement("div");
+    $(lastBar).addClass("bar");
+    $(lastBar).attr("title","Last bar");
+    $(lastBar).width("50%");
+    $("#firstBar").after(lastBar);
 
   });
 
