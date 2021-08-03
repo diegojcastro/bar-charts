8bc9b863d413146499f30a5b5fdef82d8223cedf
Diego Castro <diegojcr4@gmail.com>
2021-08-03 12:58:12 -0400

    changed graphArea function so it can be called from html

--- bar-graph-scripts.js
+++ bar-graph-scripts.js
@@ -1,3 +1,66 @@
+// Using this to test calling a function here from the html page
+function myTestFunc() {
+  $("#testFunc").html('Hello');
+}
+
+// I'm putting this outside the jquery document.ready so I can reference
+// it on the html. Is this bad form?
+function graphArea(data) {
+
+  data.reverse();
+
+  $("#firstBar").width(0);
+  $("#firstBar").attr({
+    "margin": "0px",
+    "padding": "0px"
+  });
+
+  var numBars = data.length;
+  // Quick pass to get the highest value for %size calculations
+  let maxValue = 0;
+  for (let i = 0; i < numBars; i++ ) {
+    if (data[i] > maxValue) {
+      maxValue = data[i];
+    }
+  }
+
+  let chartHeight = $(".graphArea").height();
+  // $("#placeholderText").text(chartHeight);
+  $("#placeholderText").remove();
+
+  for(let barCount = 0; barCount < numBars; barCount++) {
+    let newBar = document.createElement("div");
+    $(newBar).addClass("bar");
+    // titleNum will be assigned to "title" attribute on mouseover
+    let titleNum = data[barCount];
+    // evenly divide bars inside parent chart area
+    let percentWidth = 100/numBars;
+    $(newBar).attr({
+      "title": titleNum
+    });
+
+
+    $(newBar).width(percentWidth+"%");
+    //$(newBar).height(chartHeight);
+    $(newBar).height((data[barCount]/maxValue)*chartHeight);
+    //$(newBar).height(myData[barCount]/maxValue+"%");
+    $("#firstBar").after(newBar);
+
+  }
+
+  //Manual test of a last bar:
+  let lastBar = document.createElement("div");
+  $(lastBar).addClass("bar");
+  let percentHeight = 1.0;
+  $(lastBar).attr("title",percentHeight * chartHeight);
+  //Height looks very small. 100% of what?
+  $(lastBar).height(percentHeight * chartHeight);
+  $(lastBar).width("20%");
+
+  // Uncomment below to display test bar at start of graph
+  // $("#firstBar").after(lastBar);
+}
+
 $(document).ready(function(){
   $("#paragraphToggle").click(function(){
     $("p").toggle(200);
@@ -7,67 +70,15 @@ $(document).ready(function(){
     $("#graphTitle").text( $("#titleForm").val() );
   });
 
+
+
+
+
   // Why doesn't it let me use 'let' instead of 'var'?
   var myData = [4, 2, 3, 4, 3, 5, 6, 7, 1, 8];
   myData.push(5);
 
 
-
-
-  function graphArea(data) {
-    data.reverse();
-
-    $("#firstBar").width(0);
-    $("#firstBar").attr({
-      "margin": "0px",
-      "padding": "0px"
-    });
-
-    var numBars = myData.length;
-    // Quick pass to get the highest value for %size calculations
-    let maxValue = 0;
-    for (let i = 0; i < numBars; i++ ) {
-      if (myData[i] > maxValue) {
-        maxValue = myData[i];
-      }
-    }
-
-    let chartHeight = $(".graphArea").height();
-    // $("#placeholderText").text(chartHeight);
-    $("#placeholderText").remove();
-
-    for(let barCount = 0; barCount < numBars; barCount++) {
-      let newBar = document.createElement("div");
-      $(newBar).addClass("bar");
-      // titleNum will be assigned to "title" attribute on mouseover
-      let titleNum = myData[barCount];
-      // evenly divide bars inside parent chart area
-      let percentWidth = 100/numBars;
-      $(newBar).attr({
-        "title": titleNum
-      });
-
-
-      $(newBar).width(percentWidth+"%");
-      //$(newBar).height(chartHeight);
-      $(newBar).height((myData[barCount]/maxValue)*chartHeight);
-      //$(newBar).height(myData[barCount]/maxValue+"%");
-      $("#firstBar").after(newBar);
-
-    }
-
-    //Manual test of a last bar:
-    let lastBar = document.createElement("div");
-    $(lastBar).addClass("bar");
-    let percentHeight = 1.0;
-    $(lastBar).attr("title",percentHeight * chartHeight);
-    //Height looks very small. 100% of what?
-    $(lastBar).height(percentHeight * chartHeight);
-    $(lastBar).width("20%");
-
-    // Uncomment below to display test bar at start of graph
-    // $("#firstBar").after(lastBar);
-  }
 
   $("#getData").click(function() { graphArea(myData) } );
 
